8ba83651827888d6648e333a27fb0d689e52c248
shyustc <shyustc@163.com>
2019-07-29 13:41:59 +0000

    Update run_into_docker.sh

--- run_into_docker.sh
+++ run_into_docker.sh
@@ -3,3 +3,4 @@ docker exec -it xxx /bin/bash
 # /spinningup/spinup/utils/mpi_tools.py
 # row 31
 # args = ["mpirun", "--allow-run-as-root", "-np", str(n)]
+python -m spinup.run ppo --hid "[32,32]" --env LunarLander-v2 --exp_name installtest --gamma 0.999 --cpu 8
