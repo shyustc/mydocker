9f745b14c29847c7f1ec610c7f605a0b69c0070a
shyustc <shyustc@163.com>
2019-07-29 13:56:32 +0000

    Update run_into_docker.sh

--- run_into_docker.sh
+++ run_into_docker.sh
@@ -4,3 +4,9 @@ docker exec -it xxx /bin/bash
 # row 31
 # args = ["mpirun", "--allow-run-as-root", "-np", str(n)]
 python -m spinup.run ppo --hid "[32,32]" --env LunarLander-v2 --exp_name installtest --gamma 0.999 --cpu 8
+
+# After it finishes training, watch a video of the trained policy with
+python -m spinup.run test_policy data/installtest/installtest_s0
+
+#And plot the results with
+python -m spinup.run plot data/installtest/installtest_s0
